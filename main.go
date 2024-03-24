4ac05aa7ac60e527be92aa8761eba17a5ad0b993
Pedro Cardoso <pmtcardoso@gmail.com>
2024-03-24 22:52:32 +0000

    Change listen

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func (s *server) Divide(ctx context.Context, in *pb.OperationRequest) (*pb.Opera
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
